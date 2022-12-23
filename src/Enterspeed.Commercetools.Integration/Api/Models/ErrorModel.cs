0fe033ccf46d0616650ebab0162da614dab914d1
jesperweber <jweber@jweber.dk>
2022-12-23 11:47:35 +0100

    Removed unused namespaces

--- src/Enterspeed.Commercetools.Integration/Api/Models/ErrorModel.cs
+++ src/Enterspeed.Commercetools.Integration/Api/Models/ErrorModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Enterspeed.Commercetools.Integration.Api.Models
+﻿namespace Enterspeed.Commercetools.Integration.Api.Models
 {
     public class ErrorModel
     {
